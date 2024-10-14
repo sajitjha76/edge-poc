738c8f15a77a5b4d82c680e1785309d025014420
sajitjha76 <sajitjha@kpmg.com>
2024-10-14 13:51:08 +0530

    Update author.js

--- blocks/author/author.js
+++ blocks/author/author.js
@@ -2,12 +2,36 @@ import { fetchPlaceholders,getMetadata } from '../../scripts/aem.js';
 const placeholders = await fetchPlaceholders(getMetadata("locale"));
 console.log(placeholders);
 const { fnameKey,lnameKey,roleKey,orgKey,cntryKey,headerauthorDetails,firstName,lastName,occupation,bio,topics} = placeholders;
+let authorRow=document.createElement("tr");
+let authorCol=document.createElement("th");authorCol.appendChild(document.createTextNode(header));
+authorCol.colSpan=2;
+authorRow.append(authorCol);
 
 let firstNameRow=document.createElement("tr);
 let firstNameColl=document.createElement("td");firstNameCol1.appendChild(document.createTextNode(fnamekey));
 let firstNameCo12=document.createElement("id");firstNaneCo12.appendChild(document.createTestNode(firstName));
 firstNameRow.append(firstNameColl);  firstNameRow.append(firstNameCo12);
 
+let lirstNameRow=document.createElement("tr);
+let lirstNameColl=document.createElement("td");lirstNameCol1.appendChild(document.createTextNode(lnamekey));
+let lirstNameCo12=document.createElement("id");lirstNaneCo12.appendChild(document.createTestNode(lirstName));
+lirstNameRow.append(lirstNameColl);  lirstNameRow.append(lirstNameCo12);
+
+let roleRow=document.createElement("tr);
+let roleColl=document.createElement("td");roleCol1.appendChild(document.createTextNode(rolekey));
+let roleCo12=document.createElement("id");roleCo12.appendChild(document.createTestNode(role));
+roleRow.append(roleColl);  roleRow.append(roleCo12);
+
+let orgRow=document.createElement("tr);
+let orgColl=document.createElement("td");orgCol1.appendChild(document.createTextNode(orgkey));
+let orgCo12=document.createElement("id");orgCo12.appendChild(document.createTestNode(org));
+orgRow.append(orgColl);  orgRow.append(orgCo12);
+
+table.append(authorRow);
+table.append(firstNameRow);
+table.append(lirstNameRow);
+table.append(roleRow);
+table.append(orgRow);
 
 export default function decorate(block) {
     const headingDiv=document.createElement('div');
