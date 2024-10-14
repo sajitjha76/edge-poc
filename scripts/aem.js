695051c0e9eec1ec25d54fb6a97df7dff6848dba
sajitjha76 <sajitjha@kpmg.com>
2024-10-14 13:03:27 +0530

    Update aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -492,7 +492,11 @@ async function fetchPlaceholders(prefix = 'default') {
   window.placeholders = window.placeholders || {};
   if (!window.placeholders[prefix]) {
     window.placeholders[prefix] = new Promise((resolve) => {
-      fetch(`${prefix === 'default' ? '' : prefix}/placeholders.json`)
+      //fetch(`${prefix === 'default' ? '' : prefix}/placeholders.json`)
+      let localizedURL = new URL(window.location.origin+"/"+prefix+"/placeholders.json");
+      //console.log("prefix",prefix,(`${prefix === 'default' ? '' : prefix}/placeholders.json`));
+      //fetch(`${prefix === 'default' ? '' : prefix}/placeholders.json`)
+      fetch(localizedURL)
         .then((resp) => {
           if (resp.ok) {
             return resp.json();
