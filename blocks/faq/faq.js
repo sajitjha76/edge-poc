93509108cd00eb20f45d41d6afd41a1f9c1198b8
sajitjha76 <sajitjha@kpmg.com>
2024-11-20 17:34:26 +0530

    Update faq.js

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -5,8 +5,8 @@
 
 const elements = document.querySelectorAll(".faq-wrapper .faq h3")
 elements.forEach(element => {
-  element.addEventListener('click', function(e) {
-    const parentElement = e.parentElement; 
+  element.addEventListener('click', function() {
+    const parentElement = element.parentElement; 
     // Find the sibling of the parent element 
     const siblingOfParent = parentElement.nextElementSibling;
     console.log(siblingOfParent)
