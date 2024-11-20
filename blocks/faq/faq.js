ffc74f25190bd5ecfc4f391da7b8fd37a34ff385
sajitjha76 <sajitjha@kpmg.com>
2024-11-20 17:27:30 +0530

    Update faq.js

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -3,12 +3,12 @@
 //   console.log("Hello");
 // })
 
-const elements = document.querySelector(".faq-wrapper .faq h3")
-elements.forEach(element => {
-  // Attach a click event listener
-  element.addEventListener('click', function() {
-    alert('Element with class1 and class2 clicked!');
-  });
-});
+// const elements = document.querySelector(".faq-wrapper .faq h3")
+// elements.forEach(element => {
+//   // Attach a click event listener
+//   element.addEventListener('click', function() {
+//     alert('Element with class1 and class2 clicked!');
+//   });
+// });
 
  console.log("Hello");
