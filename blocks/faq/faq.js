bca8e9b7397eac845ba0ce2a8831af21fc02fc85
sajitjha76 <sajitjha@kpmg.com>
2024-11-20 17:21:54 +0530

    Update faq.js

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,6 +1,12 @@
-$(".faq-wrapper .faq h3").on("click" , function(){
+// $(".faq-wrapper .faq h3").on("click" , function(){
 
-  console.log("Hello");
-})
+//   console.log("Hello");
+// })
 
-// document.querySelector()
+const elements = document.querySelector(".faq-wrapper .faq h3")
+elements.forEach(element => {
+  // Attach a click event listener
+  element.addEventListener('click', function() {
+    alert('Element with class1 and class2 clicked!');
+  });
+});
