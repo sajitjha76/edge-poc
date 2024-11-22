659f9dc49398e7068f3b354d579e90c9dbd83d6a
sajitjha76 <sajitjha@kpmg.com>
2024-11-22 11:37:29 +0530

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,9 +1,8 @@
 
 import { fetchPlaceholders } from '../../scripts/aem.js';
 
-    const divElement = document.getElementsByClassName('checkbox-wrapper');
-    console.log(divElement , divElement[0]);
-  
+    const divElement = document.getElementsByClassName('checkbox-wrapper')[0];
+    console.log(divElement)  
   // Select the div element
     // const divElement = document.getElementsByClassName('checkbox-wrapper');
     //     if (divElement) {  
