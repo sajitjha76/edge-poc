c9d0271cd0c00229c8ffa9388655d540ed3fb6b4
sajitjha76 <sajitjha@kpmg.com>
2024-11-25 09:35:26 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -13,7 +13,7 @@ export default async function decorate(block) {
   if (window.location.pathname.startsWith('/fr/')) {  
     footerPagePath = '/fr/footer';
     var navX = document.querySelector('nav');
-    navx.classList.add("xxxx")
+    navX.classList.add("xxxx")
   }
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : footerPagePath;
   const fragment = await loadFragment(footerPath);
